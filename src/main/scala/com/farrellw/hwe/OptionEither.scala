b356e4972b5c42544964ca094b0987b37bf8bc0b
Will Farrell <farrell.will.g@gmail.com>
2020-10-02 16:02:05 -0500

    Solve dog age and average temperature.

--- src/main/scala/com/farrellw/hwe/OptionEither.scala
+++ src/main/scala/com/farrellw/hwe/OptionEither.scala
@@ -9,18 +9,28 @@ object OptionEither {
     Returns age of a dog when given a human age.
     Returns None if the input is None.
   */
-  def dogAge(humanAge: Option[Int]): Option[Int] = ???
+  def dogAge(humanAge: Option[Int]): Option[Int] = {
+    humanAge.map(_ * 7)
+  }
 
   /*
     Returns the total cost af any item.
     If that item has a price, then the price + 7% of the price should be returned.
   */
-  def totalCost(item: Item): Int = ???
+  def totalCost(item: Item): Option[Double] = {
+    item.price.map((x: Int) => x + (x * .07))
+  }
 
   /*
     Given a list of weather temperatures, calculates the average temperature across all weather stations.
     Some weather stations don't report temperature
     Returns None if the list is empty or no weather stations contain any temperature reading.
    */
-  def averageTemperature(temperatures: List[WeatherStation]): Option[Int] = ???
+  def averageTemperature(temperatures: List[WeatherStation]): Option[Int] = {
+    val tempsOnly = temperatures.flatMap(_.temperature)
+    tempsOnly match {
+      case Nil => None
+      case xs: List[Int] => Some(xs.sum / xs.length)
+    }
+  }
 }
