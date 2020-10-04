8a37d66813d39cda3152047e86c66b48ac9667c4
Will Farrell <farrell.will.g@gmail.com>
2020-10-04 18:53:30 -0500

    Solve miscellaneous tests.

--- src/main/scala/com/farrellw/hwe/exercises/Miscellaneous.scala
+++ src/main/scala/com/farrellw/hwe/exercises/Miscellaneous.scala
@@ -5,8 +5,12 @@ object Miscellaneous {
     Given a list of Option[Ints], returns the minimum of the Ints provided.
     If no ints are provided, return None.
    */
-  def minimum(l: List[Option[Int]]): Option[Int] = ???
-
+  def minimum(l: List[Option[Int]]): Option[Int] = {
+    l.flatten match {
+      case Nil => None
+      case xs => Some(xs.min)
+    }
+  }
 
   /*
     Checks if an attempted password is valid or invalid.
@@ -15,7 +19,17 @@ object Miscellaneous {
     A valid password returns the password back.
     An invalid password returns an exception.
    */
-  def acceptablePassword(s: String): Either[Exception, String] = ???
+  def acceptablePassword(s: String): Either[Exception, String] = {
+    if(s.toLowerCase == s){
+      Left(new Exception("No upper case letters"))
+    }else if (s.toUpperCase == s){
+      Left(new Exception("No lower case letters"))
+    } else if (s.length < 8){
+      Left(new Exception("Not long enough"))
+    } else {
+      Right(s)
+    }
+  }
 
   /*
     Checks if a string is palindrome.
