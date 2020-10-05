0f4fa9c3d3ea665bc33e57ad0b1b3d202670abe3
Will Farrell <farrell.will.g@gmail.com>
2020-10-04 20:46:10 -0500

    Solve roman numeral.

--- src/main/scala/com/farrellw/hwe/exercises/RomanNumeral.scala
+++ src/main/scala/com/farrellw/hwe/exercises/RomanNumeral.scala
@@ -1,5 +1,6 @@
 package com.farrellw.hwe.exercises
 
+// NOTE - For simplicity sake, solve assuming old roman numeral style. 4 is IIII and NOT IV
 object RomanNumeral {
   /*
     Map containing the numerical value to its roman numeral equivalent.
@@ -20,7 +21,7 @@ object RomanNumeral {
   val ROMAN_TO_INT: Map[String, Int] = Map[String, Int](
     "M" -> 1000,
     "D" -> 500,
-    "C" ->  100,
+    "C" -> 100,
     "L" -> 50,
     "X" -> 10,
     "V" -> 5,
@@ -30,17 +31,46 @@ object RomanNumeral {
   /*
     Given a roman numeral ( e.g. CCX ), returns the numerical equivalent ( e.g. 210 ).
     If the roman numeral contains any invalid roman numerals, returns None.
+    // NOTE - For simplicity sake, solve assuming old roman numeral style. 4 is IIII and NOT IV
    */
-  def convertRomanToInt(s: String): Option[Int] = ???
+  def convertRomanToInt(s: String): Option[Int] = {
+    val startingValue: Option[Int] = Some(0)
+
+    s.split("").map(ROMAN_TO_INT.get).foldLeft(startingValue)((agg: Option[Int], next: Option[Int]) => {
+      agg.flatMap(a => next.map(n => a + n))
+    })
+  }
 
   /*
     Given a complete roman numeral ( e.g. CCX ), returns the numerical equivalent ( e.g. 210 ).
     If the roman numeral contains any invalid roman numerals, return an exception
    */
-  def convertRomanToIntEither(s: String): Either[Exception, Int] = ???
+  def convertRomanToIntEither(s: String): Either[Exception, Int] = {
+    val startingValue: Either[Exception, Int] = Right(0)
+    s.split("").map(ROMAN_TO_INT.get).foldLeft(startingValue)((agg: Either[Exception, Int], next: Option[Int]) => {
+      agg.flatMap(x => {
+        next match {
+          case None => Left(new Exception("Invalid Roman Numeral"))
+          case Some(v) => Right(x + v)
+        }
+      })
+    })
+  }
 
   /*
     Given a number, return the roman numeral equivalent
    */
-  def convertIntToRoman(i: Int): Option[String] = ???
+  def convertIntToRoman(i: Int): String = {
+    val keys = INT_TO_ROMAN.keys.toList.sorted.reverse
+    keys.foldLeft((i, ""))((x: (Int, String), y: Int) => {
+      val divided = x._1 / y
+      if(divided > 0){
+        val romanNumeral = INT_TO_ROMAN(y)
+        val stringToAdd = romanNumeral * divided
+        (x._1 - (y * divided), x._2 + stringToAdd)
+      } else {
+        x
+      }
+    })._2
+  }
 }
