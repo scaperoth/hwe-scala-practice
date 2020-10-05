91fb3abafe31047a0363b04c89458e1b441dee70
Will Farrell <farrell.will.g@gmail.com>
2020-10-04 21:15:45 -0500

    Solve word count.

--- src/main/scala/com/farrellw/hwe/exercises/WordCount.scala
+++ src/main/scala/com/farrellw/hwe/exercises/WordCount.scala
@@ -4,15 +4,33 @@ object WordCount {
   /*
     Given a single sentence, return a list of words
    */
-  def splitSentenceIntoWords(sentence: String): List[String] = ???
+  def splitSentenceIntoWords(sentence: String): List[String] = {
+      sentence.split(""" """).toList
+  }
 
   /*
     Given a single sentence, return a Map of words to their counts
    */
-  def sentenceWordCount(sentence: String): Map[String, Int] = ???
+  def sentenceWordCount(sentence: String): Map[String, Int] = {
+    splitSentenceIntoWords(sentence).map(word => {
+      (word, 1)
+    }).groupBy(x => {
+      x._1
+    }).map(z => {
+      (z._1, z._2.length)
+    })
+  }
 
   /*
     Given a list of sentences, return a map of words to their counts
    */
-  def wordCount(sentences: List[String]): Map[String, Int] = ???
+  def wordCount(sentences: List[String]): Map[String, Int] = {
+    sentences.flatMap(splitSentenceIntoWords).map(word => {
+      (word, 1)
+    }).groupBy(x => {
+      x._1
+    }).map(z => {
+      (z._1, z._2.length)
+    })
+  }
 }
